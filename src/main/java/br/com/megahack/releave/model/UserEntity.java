dc679a2e7f8a209a0999f08d9e1a69195fabbc32
Almir Junior <almirjr944@gmail.com>
2020-05-02 14:47:53 -0300

    mongo db

--- src/main/java/br/com/megahack/releave/model/UserEntity.java
+++ src/main/java/br/com/megahack/releave/model/UserEntity.java
@@ -1,27 +1,44 @@
 package br.com.megahack.releave.model;
 
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.internal.util.privilegedactions.LoadClass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Entity(name = "tb_user")
 @AllArgsConstructor
-@NoArgsConstructor
+@Document(collection = "users")
 public class UserEntity implements Serializable {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="user_sequence")
-  @SequenceGenerator(name = "user_sequence", sequenceName = "user_seq")
-  private Long id;
-
-  @Column(name = "name", nullable = false, length = 100)
+  private String id;
   private String name;
+  private String cpf;
+  private LocalDate birthday;
+  private Gender gender;
+  private UserType type;
+  private String photoUrl;
+  private String password;
+  private LocalDateTime createdDate;
+
+  private enum Gender{
+    MAN, WOMAN
+  }
+  private enum UserType{
+     USER, PROVIDER
+  }
+
+  public UserEntity (){
+    this.createdDate = LocalDateTime.now();
+  }
+
 }
--- src/main/java/br/com/megahack/releave/repository/UserRepository.java
+++ src/main/java/br/com/megahack/releave/repository/UserRepository.java
@@ -2,11 +2,12 @@ package br.com.megahack.releave.repository;
 
 import br.com.megahack.releave.model.UserEntity;
 import java.util.List;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import java.util.UUID;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
+public interface UserRepository extends MongoRepository<UserEntity, String> {
 
   List<UserEntity> findAll();
 
